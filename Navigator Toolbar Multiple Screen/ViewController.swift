f0704e65a5847d2a5ab8cf071b4896e41c1fc9ca
student09 <kannikar@snru.ac.th>
2017-06-14 16:51:42 +0700

    Project success...

--- Navigator Toolbar Multiple Screen/ViewController.swift
+++ Navigator Toolbar Multiple Screen/ViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Implicit
+    var intNumber = 1
+    
+    @IBAction func increaseNumber(_ sender: Any) {
+        
+        showMessage(strMessage: "Click Increase Number...")
+        intNumber += 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+        showMessage(strMessage: String  (intNumber))
+    }
+    
+    @IBAction func decreaseNumber(_ sender: Any) {
+        
+        showMessage(strMessage: "Click Decrease Number...")
+        //showNumber(intNumber: intNumber)
+        
+        intNumber -= 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+        
+        showMessage(strMessage: String  (intNumber))
+    }
+    
+    @IBAction func resetNumber(_ sender: Any) {
+        
+        showMessage(strMessage: "Click Reset Number...")
+        //showNumber(intNumber: intNumber)
+        intNumber = 1
+        intNumber = checkNumber(intNumber: intNumber)
+        showNumber(intNumber: intNumber)
+        showMessage(strMessage: String  (intNumber))
+    }
+
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    func showNumber(intNumber: Int) -> Void {
+        let strNumber = String(intNumber)
+        numberLabel.text = strNumber
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } // end main method
+    
+    
+    
+    func checkNumber(intNumber: Int) -> Int {
+        var intResult = intNumber
+        
+        // At 11
+        if intNumber == 11 {
+            intResult = -10
+        }
+        
+        // At -11
+        if intNumber == -11 {
+            intResult = 10
+        }
+        return intResult
+    }
+    
+    
+    func showMessage(strMessage: String) -> Void {
+        print("strMessage ==> \(strMessage)")
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
